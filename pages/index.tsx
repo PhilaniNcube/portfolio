8746f382861f6819bafa49831b4fb7f37cebb144
Philani Ncube <ncbphi001@gmail.com>
2022-10-26 22:51:42 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,103 @@
+import {
+  useScroll,
+  motion,
+  useMotionValue,
+  useTransform,
+  useMotionTemplate,
+} from "framer-motion";
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
+import { Fragment } from 'react'
+
+let clamp = (number: number, min: number, max: number) =>
+  Math.min(Math.max(number, min), max);
+
+function useBoundedScroll(bounds:number) {
+  let { scrollY } = useScroll();
+  let scrollYBounded = useMotionValue(0)
+  let scrollYBoundedProgress = useTransform(scrollYBounded, [0, bounds], [0,1]);
+
+  useEffect(() => {
+    return scrollY.onChange((current) => {
+      let previous = scrollY.getPrevious();
+      let diff = current - previous;
+      let newScrollYBounded = scrollYBounded.get() + diff;
+
+      scrollYBounded.set(clamp(newScrollYBounded, 0, bounds));
+    });
+  }, [bounds, scrollY, scrollYBounded]);
+
+  return { scrollYBounded, scrollYBoundedProgress };
+}
+
+
+
 
 const Home: NextPage = () => {
+
+let { scrollYBoundedProgress } = useBoundedScroll(300);
+let height = useTransform(scrollYBoundedProgress, [0, 1], [80, 50]);
+let opacity = useTransform(scrollYBoundedProgress, [0, 1], [1, 0]);
+let scale = useTransform(scrollYBoundedProgress, [0, 1], [1, 0.9]);
+
+
+
+
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <Fragment>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title> Philani Ncube | Developer Portfolio </title>
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+      <div className="mx-auto flex w-full max-w-7xl flex-1 overflow-hidden text-slate-600">
+        <div className="z-0 flex-1">
+          <motion.header
+            style={{ height, backgroundColor: useMotionTemplate`rgb(255,255,255 / ${useTransform(scrollYBoundedProgress, [0,1],[1,0.1])})` }}
+            className="flex fixed inset-x-0 shadow-md backdrop-blur-md h-20"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+            <div className="mx-auto flex w-full items-center justify-between px-8">
+              <motion.p style={{scale}} className="flex origin-left items-center text-xl font-semibold uppercase">
+                <span className="-ml-1.5 inline-block -rotate-90 text-[10px] leading-[0]">
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+                </span>
+                <span className="-ml-1 text-2xl tracking-[-.075em]">
+                  Philani Ncube
+                </span>
+              </motion.p>
+              <motion.nav style={{opacity}} className="flex space-x-4 text-xs font-medium text-slate-400">
+                <a href="#">News</a>
+                <a href="#">Sports</a>
+                <a href="#">Culture</a>
+              </motion.nav>
+            </div>
+          </motion.header>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <main className="px-8 pt-28">
+            <h1 className="h-10 w-4/5 rounded bg-slate-200 text-2xl font-bold" />
+            <div className="mt-8 space-y-6">
+              {[...Array(2).keys()].map((i) => (
+                <div key={i} className="space-y-2 text-sm">
+                  <p className="h-4 w-5/6 rounded bg-slate-200" />
+                  <p className="h-4 rounded bg-slate-200" />
+                  <p className="h-4 w-4/6 rounded bg-slate-200" />
+                </div>
+              ))}
+              <div className="h-64 rounded bg-slate-200"></div>
+              {[...Array(90).keys()].map((i) => (
+                <div key={i} className="space-y-2 text-sm">
+                  <p className="h-4 w-5/6 rounded bg-slate-200" />
+                  <p className="h-4 rounded bg-slate-200" />
+                  <p className="h-4 w-4/6 rounded bg-slate-200" />
+                </div>
+              ))}
+            </div>
+          </main>
         </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
+      </div>
+    </Fragment>
+  );
 }
 
 export default Home

270cdd8033a24685cfbc45537033ebecce109577
Philani Ncube <ncbphi001@gmail.com>
2022-10-29 20:19:56 +0200

    transition color on hover

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -67,7 +67,7 @@ const Timeline = () => {
       technology: "Wordpress",
       details:
         "I started getting interested in web development and started playing around with Wordpress. I was attracted to the low barrier to entry because at first it seemed I did not need to know any actual programming language to start",
-      icon: [<DiWordpress className="h-6 w-6 text-blue-600" />],
+      icon: [<DiWordpress className="h-6 w-6 text-blue-100" />],
     },
     {
       id: 2,
@@ -76,9 +76,9 @@ const Timeline = () => {
       details:
         "As one of the more tech savy people working at the Bookshop I used to work at, I was tasked with managing our newly built Magento website. I was the one to speak to the developers we had contracted to build the website. It was also at this point that I started to learn HTML & CSS in order to make updates to our company website without having to contact the developers.",
       icon: [
-        <DiMagento className="h-6 w-6 text-blue-600" />,
-        <DiHtml5 className="h-6 w-6 text-blue-600" />,
-        <DiCss3 className="h-6 w-6 text-blue-600" />,
+        <DiMagento className="h-6 w-6 text-blue-100" />,
+        <DiHtml5 className="h-6 w-6 text-blue-100" />,
+        <DiCss3 className="h-6 w-6 text-blue-100" />,
       ],
     },
     {
@@ -88,8 +88,8 @@ const Timeline = () => {
       details:
         "Once I had a grasp of HTML & CSS, the natural thin was to add Javascript to my toolbelt. It is as this point that I started to realise that web development was the career I wanted to pursue going forward. During this time I was also still managing our company Magento website and taking up more and more responsibility on updating the site ysing the Magento block editor. I also built out 2 Wordpress websites for other business units in our group of companies.",
       icon: [
-        <DiMagento className="h-6 w-6 text-blue-600" />,
-        <DiJsBadge className="h-6 w-6 text-blue-600" />,
+        <DiMagento className="h-6 w-6 text-blue-100" />,
+        <DiJsBadge className="h-6 w-6 text-blue-100" />,
       ],
     },
     {
@@ -99,9 +99,9 @@ const Timeline = () => {
       details:
         "At this point I started freelancing and the Covid19 hit which and the Bookstore I used to work for since 2014 closed down. There I went full-time as a freelancer. The first project I took was create a React based ecommerce store that served the same customers as the University Bookstore I used to work for. In working as a freelancer I started working with different REST APIs like SendGrid, Mailchimp & MongoDB",
       icon: [
-        <DiReact className="h-6 w-6 text-blue-600" />,
-        <TbApi className="h-6 w-6 text-blue-600" />,
-        <SiApollographql className="h-6 w-6 text-blue-600" />,
+        <DiReact className="h-6 w-6 text-blue-100" />,
+        <TbApi className="h-6 w-6 text-blue-100" />,
+        <SiApollographql className="h-6 w-6 text-blue-100" />,
       ],
     },
     {
@@ -111,9 +111,9 @@ const Timeline = () => {
       details:
         "At the begining of 2021 I decided to rewrite my ecommerce website with NextJS so that I could take advantage of better SEO and I moved from MongoDB to Supabase as the database. Since then Supabase/Postgres has been my pick for the backend & database technology for the freelance projects I have been taking on. I have also decided to use Typescript as the default language I use in my projects.",
       icon: [
-        <SiTypescript className="h-6 w-6 text-blue-600" />,
-        <SiPostgresql className="h-6 w-6 text-blue-600" />,
-        <SiMongodb className="h-6 w-6 text-blue-600" />,
+        <SiTypescript className="h-6 w-6 text-blue-100" />,
+        <SiPostgresql className="h-6 w-6 text-blue-100" />,
+        <SiMongodb className="h-6 w-6 text-blue-100" />,
       ],
     },
   ];
@@ -123,7 +123,7 @@ const Timeline = () => {
   return (
     <section ref={container} className="py-8 mt-24" id="journey">
       <div className="max-w-7xl mx-auto px-4">
-        <h2 className="text-2xl md:text-3xl text-blue-600 font-medium text-center">
+        <h2 className="text-2xl md:text-3xl text-blue-600 font-medium font-mono text-center">
           My Web Development Journey
         </h2>
         <div className="mt-4 gap-3 max-w-4xl mx-auto overflow-x-hidden">
--- components/TimelineItem.tsx
+++ components/TimelineItem.tsx
@@ -43,17 +43,21 @@ const TimelineItem = ({item, index}:ComponentProps) => {
     <motion.div
       initial={{ opacity: 0, translateX: index % 2 === 0 ? -100 : 100 }}
       whileInView={{ opacity: 1, translateX: 0 }}
-      transition={{ duration: 0.8, delay: 0.5 }}
+      whileHover={{ backgroundColor: "rgb(37 99 235)" }}
+      transition={{ duration: 0.8, delay: 0.1, delayChildren: 0.3 }}
       ref={itemRef}
-      className="odd:clear-right overflow-x-hidden odd:float-left even:float-right even:clear-left w-full md:w-2/3 my-8 bg-slate-100 rounded-lg shadow-lg p-4"
+      className="odd:clear-right hover:text-white group overflow-x-hidden odd:float-left even:float-right even:clear-left w-full md:w-2/3 my-8 bg-slate-800 rounded-lg shadow-lg p-4"
     >
-      <h3 className="text-xl font-serif flex flex-col text-slate-600 font-bold">
-        <span className="pr-2">{item.technology}</span> <span className="text-sm">{item.date}</span>
+      <h3 className="text-xl font-serif flex flex-col group-hover:text-white text-slate-200 font-bold">
+        <span className="pr-2">{item.technology}</span>{" "}
+        <span className="text-sm">{item.date}</span>
       </h3>
-      <p className="font-mono mt-1 text-slate-800 text-base">{item.details}</p>
+      <p className="font-mono mt-1 text-slate-100 group-hover:text-white text-base">
+        {item.details}
+      </p>
       <motion.div className="flex space-x-3 mt-2">
         {item.icon?.map((icon, i) => (
-          <div key={i}>{icon}</div>
+          <div className="group-hover:text-white" key={i}>{icon}</div>
         ))}
       </motion.div>
     </motion.div>

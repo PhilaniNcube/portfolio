0b8084b33c58bc0e5b9d9a7213b2202324d08b78
Philani Ncube <ncbphi001@gmail.com>
2022-10-30 09:25:03 +0200

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,15 @@ import '../styles/globals.css'
 import Header from '../components/Header'
 import type { AppProps } from 'next/app'
 import { Fragment } from 'react';
+import { useEffect } from "react";
+import analytics from "../utils/analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+   useEffect(() => {
+     analytics.page();
+   }, []);
+
   return (
     <Fragment>
       <Header />
---
+++ utils/analytics.js
@@ -0,0 +1,17 @@
+import Analytics from "analytics";
+// ignore this
+import googleTagManager from "@analytics/google-tag-manager";
+
+const GTM_CONTAINER_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
+
+const analytics = Analytics({
+  app: "hotwater24", // Call this whatever you like.
+  plugins: [
+    googleTagManager({
+      containerId: GTM_CONTAINER_ID,
+      enabled: true,
+    }),
+  ],
+});
+
+export default analytics;

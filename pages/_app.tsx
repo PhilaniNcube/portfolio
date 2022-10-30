e2fda8020c9a9336d4a1f7b5e2514c9a774b96bd
Philani Ncube <ncbphi001@gmail.com>
2022-10-30 09:36:49 +0200

    nextjs analytics example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,27 @@ import type { AppProps } from 'next/app'
 import { Fragment } from 'react';
 import { useEffect } from "react";
 import analytics from "../utils/analytics";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
 
    useEffect(() => {
      analytics.page();
    }, []);
+
+   const router = useRouter();
+   useEffect(() => {
+     const handleRouteChange = (url:string) => {
+       gtag.pageview(url);
+     };
+     router.events.on("routeChangeComplete", handleRouteChange);
+     router.events.on("hashChangeComplete", handleRouteChange);
+     return () => {
+       router.events.off("routeChangeComplete", handleRouteChange);
+       router.events.off("hashChangeComplete", handleRouteChange);
+     };
+   }, [router.events]);
 
   return (
     <Fragment>
